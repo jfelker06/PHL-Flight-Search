ffe76483ff45611f76a410ae16c363293e578957
Joshua Felker <joshua.felker@gmail.com>
2014-03-09 15:42:06 -0400

    added accordian view to multiple search results

--- js/flightSearch.js
+++ js/flightSearch.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
     var divClone = $("#flights").clone();
+    var matchCount = 0;
     $(".btnSubmit").click(function() {
       $("#flights").replaceWith(divClone);
       var direction = $("select#directionSelect").val();
       var url = 'http://flightinfo.phlapi.com/direction/' + direction;
+      matchCount = 0;
 		  $.ajax({
 			  dataType: 'jsonp',
             type: 'GET',
@@ -36,15 +38,23 @@ $(document).ready(function() {
                     }
                  	 }  
 
-                   if (text.length < 10) {
+                   if (matchCount == 0) {
                     text = '<p>No flights found. Try refining your search criteria</p>';
                    } 
+                   else if (matchCount > 1) {
+                    text = '<div id="accordion">' + text + '</div>';
                     document.getElementById('flights').innerHTML = text;
+                    $( "#accordion" ).accordion({collapsible: true, active: false});
+                   }
+                   else {
+                    document.getElementById('flights').innerHTML = text;
+                   }
                 }
       });
     });
 
       function buildFlightList(fl) {
+        matchCount = matchCount + 1;
         var airport = '';
         var airline = '';
         var scheduledTime = '';
@@ -56,13 +66,13 @@ $(document).ready(function() {
         var estTimeClass = '';
         airline = airlineCode(fl.airline);
         if (fl.direction == 'Departure') {
-          airport = 'Destination:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
-          scheduledTime = 'Scheduled Departure: ';
-          estTime = 'Estimated Departure: ';
+          airport = 'Destination:&nbsp;&nbsp;&nbsp;&nbsp;';
+          scheduledTime = 'Schd Departure: ';
+          estTime = 'Est Departure:&nbsp;&nbsp;';
         } else {
-          airport = 'Origin:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
-          scheduledTime = 'Scheduled Arrival:&nbsp;&nbsp;&nbsp;';
-          estTime = 'Estimated Arrival:&nbsp;&nbsp;&nbsp;';
+          airport = 'Origin:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+          scheduledTime = 'Schd Arrival:&nbsp;&nbsp;&nbsp;';
+          estTime = 'Est Arrival:&nbsp;&nbsp;&nbsp;&nbsp;';
         }
         statusClass = setStatusClass(fl.status, valScheduledTime, valEstTime);
         valScheduledTime = timeFormat(fl.scheduledDateTime);
@@ -78,10 +88,8 @@ $(document).ready(function() {
           estTimeClass = "estTime";
         }
 
-        
-
-        entry = '<ul class="flight"><li>Flight Number:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="flightNumber">' + fl.flightNumber + '</span></li><li>Airline:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="airline">' + airline + '</span></li> <li>Direction:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="direction">' + fl.direction + '</span></li> <li>Flight Type:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="flightType">' + fl.flightType +'</span></li> <li>' + airport +'<span  class="airport">'+ fl.airport + '</span></li> <li>'+ scheduledTime +'<span class="scheduledTime">' + valScheduledTime + '</span></li> <li>' + estTime + '<span class="'+ estTimeClass + '">' + valEstTime +'</span></li><li>Gate:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="gate">' + fl.gate + '</span></li> <li>Status:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="'+ statusClass +'">' + fl.status + '</span></li> <li>Terminal:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="terminal">' +fl.terminal + '</span></li> <li>Security Check:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="security">' + fl.security + '</span></li> <li>Garage:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="garage">' + fl.garage + '</span></li> <li>Septa Stop:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="septa">' + fl.septa + '</span></li></ul>';
-
+        entry = '<h3 class="ui-accordion-header">' + airline + ' Flight ' + fl.flightNumber + '<br/>' + fl.airport + '</h3> <div class="flight"><p class="'+ statusClass +'">' + fl.status + '</p><ul><li>Direction:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="direction">' + fl.direction + '</span></li> <li>Flight Type:&nbsp;&nbsp;&nbsp;&nbsp;<span class="flightType">' + fl.flightType +'</span></li> <br/> <li>'+ scheduledTime +'<span class="scheduledTime">' + valScheduledTime + '</span></li> <li>' + estTime + '<span class="'+ estTimeClass + '">' + valEstTime +'</span></li><br/><li>Gate:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="gate">' + fl.gate + '</span></li> <li>Terminal:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="terminal">' +fl.terminal + '</span></li> <li>Security Check:&nbsp;<span class="security">' + fl.security + '</span></li> <li>Garage:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="garage">' + fl.garage + '</span></li> <li>Septa Stop:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="septa">' + fl.septa + '</span></li></ul></div>';
+      
         return entry;
       }
 
